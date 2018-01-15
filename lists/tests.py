5c12a8067bb32de7c68bafb68308282de0ab9b8a
Enrico Savioli <enrico.savioli@datalogic.com>
2018-01-15 11:30:09 +0100

    Redirect after POST, show all items in template.

--- lists/tests.py
+++ lists/tests.py
@@ -9,15 +9,39 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         item_text = 'A new list item'
-        response = self.client.post(
+        response = self.client.post('/', data={'item_text' : item_text})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, item_text)
+
+    def test_redirects_after_POST(self):
+        item_text = 'A new list item'
+        response = self.client.post('/', data={'item_text' : item_text})
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        item_text = 'A new list item'
+        response = self.client.get(
             '/',
             data =
                 {
                 'item_text' : item_text
                 }
             )
-        self.assertIn(item_text, response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_lists_items(self):
+        Item.objects.create(text='item1')
+        Item.objects.create(text='item2')
+
+        response = self.client.get('/')
+
+        self.assertIn('item1', response.content.decode())
+        self.assertIn('item2', response.content.decode())
+
 
     def test_saving_and_retrieving_items(self):
         first_item_text = 'Item 1'
