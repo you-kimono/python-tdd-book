d3c72509688c60abc8480713ae55b013583eb55a
Enrico Savioli <enrico.savioli@datalogic.com>
2018-01-15 09:56:15 +0100

    Refactor functional_tests.py, add a method that checks for a row in a list.

--- lists/tests.py
+++ lists/tests.py
@@ -7,3 +7,14 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_can_save_a_POST_request(self):
+        item_text = 'A new list item'
+        response = self.client.post(
+            '/',
+            data =
+                {
+                'item_text' : item_text
+                }
+            )
+        self.assertIn(item_text, response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
