b6310099d59ecd9cdb55d5c3f9aa256d870e907b
Enrico Savioli <enrico.savioli@datalogic.com>
2018-01-15 10:12:17 +0100

    First database model.

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -18,3 +18,23 @@ class HomePageTest(TestCase):
             )
         self.assertIn(item_text, response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_saving_and_retrieving_items(self):
+        first_item_text = 'Item 1'
+        second_item_text = 'Item 2'
+
+        first_item = Item()
+        first_item.text = first_item_text
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = second_item_text
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, first_item_text)
+        self.assertEqual(second_saved_item.text, second_item_text)
