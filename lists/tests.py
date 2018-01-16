43acdb543ad71e5ffbf2a19b7aebc4b58bd119a6
Enrico Savioli <enrico.savioli@datalogic.com>
2018-01-16 10:22:25 +0100

    Refactor code and tests. Functionalities preserved.

--- lists/tests.py
+++ lists/tests.py
@@ -7,21 +7,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        item_text = 'A new list item'
-        response = self.client.post('/', data={'item_text' : item_text})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, item_text)
-
-    def test_redirects_after_POST(self):
-        item_text = 'A new list item'
-        response = self.client.post('/', data={'item_text' : item_text})
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
     def test_only_saves_items_when_necessary(self):
         item_text = 'A new list item'
         response = self.client.get(
@@ -71,3 +56,19 @@ class ItemModelTest(TestCase):
 
         self.assertEqual(first_saved_item.text, first_item_text)
         self.assertEqual(second_saved_item.text, second_item_text)
+
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        item_text = 'A new list item'
+        response = self.client.post('/lists/new', data={'item_text' : item_text})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, item_text)
+
+    def test_redirects_after_POST(self):
+        item_text = 'A new list item'
+        response = self.client.post('/lists/new', data={'item_text' : item_text})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
