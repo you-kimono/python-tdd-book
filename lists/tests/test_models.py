0a7e4b7a31edcfdc77da76c12e0287c26f92d1fe
Enrico Savioli <savio@mentalsmash.org>
2018-03-05 15:08:40 +0100

    Split out unit tests into two files

---
+++ lists/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from lists.models import Item
+from lists.models import List
+
+class ListAndItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item_text = 'Item 1'
+        second_item_text = 'Item 2'
+
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = first_item_text
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = second_item_text
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, first_item_text)
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, second_item_text)
+        self.assertEqual(second_saved_item.list, list_)
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -49,39 +49,6 @@ class ListViewTest(TestCase):
         self.assertEqual(response.context['list'], correct_list)
 
 
-class ListAndItemModelTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        first_item_text = 'Item 1'
-        second_item_text = 'Item 2'
-
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = first_item_text
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = second_item_text
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-
-        self.assertEqual(first_saved_item.text, first_item_text)
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, second_item_text)
-        self.assertEqual(second_saved_item.list, list_)
-
-
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
